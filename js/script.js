2784467d1035935006cf17366a3abb10808fac5e
(s) Kyra Boyle <klboyle@pm7-0085.uopnet.plymouth.ac.uk>
2018-03-22 14:58:21 +0000

    10 commit

--- js/script.js
+++ js/script.js
@@ -9,20 +9,6 @@ function currentYear(){
   autoDate.textContent = date.getFullYear();
 };
 
-function into(){
-  var mapOptions = {
-    center : new gogle.maps.latLng(50.376347, -4.139822), mapTypeId : google.maps.MapTypeId.ROADMAP, zoom: 13
-
-  };
-  var venueMap;
-  venueMap = new google.maps.Map(document.getElementById('map'),mapOptions);
-}
-
-function loadScript() {
- var script = document.createElement('script');
- script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyAJAXVUp8P1Au_kX19qQ3tE7UOzRO59OtE&callback=initMap';
- document.body.appendChild(script);
-}
-//window.onload = loadscript;
-
-console.log("map")
+$(document).ready(function () {
+    $('div.hidden').fadeIn(1800).removeClass('hidden');
+});
